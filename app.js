58b85ecc52b2327992eaffce53168a10355a6b33
minhduc290613 <134067529+minhduc290613@users.noreply.github.com>
2026-07-09 22:54:48 +0700

    Update JSON URL in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // ĐƯỜNG DẪN FILE JSON TRÊN GITHUB CỦA BẠN (Thay đổi thông tin tài khoản của bạn tại đây)
-    const JSON_URL = "https://vandekn.qzz.io/PRT-Store/apps.json";
+    const JSON_URL = "https://vandekn.qzz.io/PRT-Store/app.json";
 
     const appGrid = document.querySelector(".app-grid");
     const downloadBtn = document.getElementById("download-btn");
